f05cee086f993c69712316c440d3fab7581f17d0
alroy214 <alroy214@gmail.com>
2021-06-03 16:47:43 +0300

    Added lists to fragments.

--- app/src/main/java/com/hack/idc/ramen/ui/participants/ParticipantsFragment.java
+++ app/src/main/java/com/hack/idc/ramen/ui/participants/ParticipantsFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -12,7 +13,12 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.hack.idc.ramen.R;
 import com.hack.idc.ramen.databinding.FragmentDashboardBinding;
+import com.hack.idc.ramen.person.Person;
+import com.hack.idc.ramen.person.PersonAdapter;
+
+import java.util.ArrayList;
 
 public class ParticipantsFragment extends Fragment {
 
@@ -24,6 +30,16 @@ public class ParticipantsFragment extends Fragment {
         View root = binding.getRoot();
 
         final TextView textView = binding.textDashboard;
+
+        ListView listView = root.findViewById(R.id.listView);
+        ArrayList<Person> arr = new ArrayList<>();
+        arr.add(new Person(R.drawable.strawhat, "monket.D Luffy", "Bounty - 1.5B$"));
+        arr.add(new Person(R.drawable.strawhat, "Roronoa Zoro", "Bounty - 500M$"));
+        arr.add(new Person(R.drawable.strawhat, "Vinsmoke Sanji", "Bounty - 700M$"));
+
+        PersonAdapter personAdapter = new PersonAdapter(requireContext(), R.layout.list_row, arr);
+
+        listView.setAdapter(personAdapter);
         return root;
     }
 
